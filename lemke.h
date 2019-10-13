ed8caf6143c82493df6e3d96d825d4757c8783d3
wangkuner <wangkuner@outlook.com>
2019-10-13 11:17:22 +0800

    delete nonuse data structures

--- lemke.h
+++ lemke.h
@@ -1,20 +1,8 @@
 #ifndef _LEMKE_H
 #define _LEMKE_H
 
-enum class Base_Type
-{
-    zero = 0,
-    one = 1
-};
-
 typedef double _real_lemke;
 typedef int _int_lemke;
-
-struct Base_Variable
-{
-    Base_Type type;
-    _int_lemke id;
-};
 
 int Lemke(const _int_lemke n, const _real_lemke *M, const _real_lemke *q,
           const _int_lemke max_iter, _int_lemke &num_iter,
